76613b52e51dca83a674620f0d4d23debac9fcb0
Alex Kesling <akdom2001@gmail.com>
2012-04-13 16:22:35 -0400

    Added ability to render portion of SVG

--- viewport.js
+++ viewport.js
@@ -1,3 +1,15 @@
 $().ready(function () {
-    canvg('viewport', 'assets/tracks/simple_path.svg', {ignoreMouse:true, ignoreAnimation:true, ignoreDimensions:true});
+    draw_viewport(0,0,100,100);
 });
+
+$("#set-xy").click(function () {
+    var x = $("#x-coord").val();
+    var y = $("#y-coord").val();
+    draw_viewport(x,y,100,100);
+});
+
+function draw_viewport(x, y, width, height) {
+    var track = $("#track")[0];
+    var viewport = $("#viewport")[0];
+    viewport.getContext('2d').drawImage(track,x,y,100,100,0,0,500,500);
+}
