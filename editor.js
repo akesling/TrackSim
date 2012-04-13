c6adb78ee302573dc05cf32b82a1fcd94c6b03b3
mgyenik <m@m-air.(none)>
2012-04-13 17:39:50 -0400

    Added syntax highlighting to Ace.

---
+++ editor.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var editor = ace.edit("setup-editor");
+    editor.getSession().setMode("ace/mode/javascript");
+});
+
+$(document).ready(function() {
+    var editor = ace.edit("loop-editor");
+    editor.getSession().setMode("ace/mode/javascript");
+});
