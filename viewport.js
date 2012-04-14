6bbb44099522f647c1814c9767757d328613c8d1
Alex Kesling <akdom2001@gmail.com>
2012-04-14 18:48:51 -0400

    Adjusted some vp params and fixed editor position.

--- viewport.js
+++ viewport.js
@@ -1,15 +1,36 @@
-$().ready(function () {
-    draw_viewport(0,0,100,100);
+/******************************************************************************/
+/** Viewport Handling *********************************************************/
+/******************************************************************************/
+
+ds = 100
+
+$(document).ready(function () {
+    draw_viewport(0,0,ds,ds);
 });
 
 $("#set-xy").click(function () {
+    var viewport = $("#viewport")[0].getContext('2d');
+    viewport.clearRect(0,0,500,500);
     var x = $("#x-coord").val();
     var y = $("#y-coord").val();
-    draw_viewport(x,y,100,100);
+    draw_viewport(x,y,ds,ds);
 });
 
 function draw_viewport(x, y, width, height) {
     var track = $("#track")[0];
-    var viewport = $("#viewport")[0];
-    viewport.getContext('2d').drawImage(track,x,y,100,100,0,0,500,500);
+    var viewport = $("#viewport")[0].getContext('2d');
+    viewport.drawImage(track,x,y,ds,ds,0,0,500,500);
+}
+
+/******************************************************************************/
+/** Car object framework ******************************************************/
+/******************************************************************************/
+
+var car = {}
+
+car.get_sensors = function() {
+    var viewport = $("#viewport")[0].getContext('2d');
+    //viewport.getImageData(,
+
+    return "foo";
 }
